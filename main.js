dd6f733ffa362ab3e9a53d3bbb70bcfb85747b27
LimboLam <liam.xiong24@student.cart.org>
2025-01-30 08:56:59 -0800

    stuff

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-let num1 = parseInt(document.getElementById(`inputNumOne`));
-let num2 = parseInt(document.getElementById(`inputNumTwo`));
-let operator = document.getElementById(`inputOperator`);
+let inputNumOne = parseInt(document.getElementById(`inputNumOne`));
+let num1 = inputNumOne;
+let inputNumTwo = parseInt(document.getElementById(`inputNumTwo`));
+let num2 = inputNumTwo;
+let inputOperator = document.getElementById(`inputOperator`);
+let operator = inputOperator;
 let button = document.querySelector(`button`);
 
+if (num1 === "null" || num2 === "null" || operator === "null")
+    alert(`Enter a number or operator`)
+
 button.addEventListener(`click`, () => {
-    if (operator == `+`) {
-        let result = num1 + num2;
-        alert(result);
+    let result;
+    switch (operator) {
+        case `+`:
+            result = num1 + num2;
+            break;
+        case `-`:
+            result = num1 - num2;
+            break;
+        case `*`:
+            result = num1 * num2;
+            break;
+        case `/`:
+            if (num2 === 0) {
+                alert(`Divide by 0 error`);
+            }
+            result = num1 / num2;
+            break;
+        default:
+            alert(`Enter a valid operator`);
+            result = `null`;
     }
-    else if (operator == `-`) {
-        result = num1 - num2;
-        alert(result);
-    }
-    else if (operator == `*`) {
-        result = num1 * num2;
-        alert(result);
-    }
-    else if (operator == `/`) {
-        result = num1 / num2;
-        alert(result);
-    }
-    else {
-        alert(`Enter a valid operator`);
-    }
+
+    document.write(`${num1} ${operator} ${num2} = ${result}`);
 })
