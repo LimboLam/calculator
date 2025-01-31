42550513b5631689c738e41ddc518fe3bb63e521
LimboLam <liam.xiong24@student.cart.org>
2025-01-31 09:06:19 -0800

    push

--- main.js
+++ main.js
@@ -1,36 +1,40 @@
-let inputNumOne = parseInt(document.getElementById(`inputNumOne`));
-let num1 = parseInt(prompt(`Enter the 1st number:`));
-let inputNumTwo = parseInt(document.getElementById(`inputNumTwo`));
-let num2 = parseInt(prompt(`Enter the 2nd number:`));
-let inputOperator = document.getElementById(`inputOperator`);
-let operator = prompt(`Enter an operator (+, -, *, /):`);
-let button = document.querySelector(`button`);
+calculations();
 
-if (num1 === NaN || num2 === NaN || operator === null)
-    alert(`Enter a number or an operator`)
-
-// button.addEventListener(`click`, () => {
+function calculations() {
+    let num1 = parseInt(prompt(`Enter the 1st number:`));
+    let num2 = parseInt(prompt(`Enter the 2nd number:`));
+    let operator = prompt(`Enter an operator (+, -, *, /):`);
     let result;
-    switch (operator) {
-        case `+`:
-            result = num1 + num2;
-            break;
-        case `-`:
-            result = num1 - num2;
-            break;
-        case `*`:
-            result = num1 * num2;
-            break;
-        case `/`:
-            if (num2 === 0) {
-                alert(`Divide by 0 error`);
-            }
-            result = num1 / num2;
-            break;
-        default:
-            alert(`Enter a valid operator`);
-            result = null;
+    if (num1 === NaN || num2 === NaN) {
+        alert(`Enter a valid number`);
+        calculations();
+    } else {
+        switch (operator) {
+            case `+`:
+                result = num1 + num2;
+                break;
+            case `-`:
+                result = num1 - num2;
+                break;
+            case `*`:
+                result = num1 * num2;
+                break;
+            case `/`:
+                if (num2 === 0) {
+                    alert(`Divide by 0 error`);
+                    calculations();
+                }
+                else {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                alert(`Enter a valid operator`);
+                result = null;
+                calculations();
+        }
     }
 
     document.write(`<h1>${num1} ${operator} ${num2} = ${result}<h1>`);
-// })
+    return;
+}
